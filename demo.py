a679ffc07aae8bb35a3a627429ea6306c3fe136a
Justin Liang <justinliang1020@gmail.com>
2021-11-23 14:34:10 -0500

    Create outline of code

---
+++ demo.py
@@ -0,0 +1,9 @@
+#create dummy folder
+
+#variable path
+
+#get all file names in path
+
+#parse through each file name, classifying into "valid" or "invalid"
+
+#print out valid types as valid, invalid types as invalid
