5f5efabaf6d9736e8a86cb02e675ac01b9e5ea46
Justin Liang <justinliang1020@gmail.com>
2022-02-16 16:17:31 -0500

    created basic connection

--- sql_connection.py
+++ sql_connection.py
@@ -1,15 +1,34 @@
 import mysql.connector
 from mysql.connector import errorcode
 
-try:
-  cnx = mysql.connector.connect(user='scott',
-                                database='employ')
-except mysql.connector.Error as err:
-  if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
-    print("Something is wrong with your user name or password")
-  elif err.errno == errorcode.ER_BAD_DB_ERROR:
-    print("Database does not exist")
-  else:
-    print(err)
-else:
-  cnx.close()
+# how to run:
+# 1. run CS33311.sql in mysql workbench
+# 2. create a custom user with the credentials:
+#   user: 'cs3311_admin'
+#   password: 'password'
+#   administrative roles: DBA
+#   schema privileges: add entry -> schema 'CS 3311', Select "ALL" privileges
+
+# database connection config info
+config = {
+  'user': 'cs3311_admin',
+  'password': 'password',
+  'host': '127.0.0.1',
+  'database': 'CS3311'
+}
+
+def connect():
+    try:
+        cnx = mysql.connector.connect(**config)
+    except mysql.connector.Error as err:
+        if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+            print("Something is wrong with your user name or password")
+        elif err.errno == errorcode.ER_BAD_DB_ERROR:
+            print("Database does not exist")
+        else:
+            print(err)
+
+    print('Database connection:', cnx)
+
+def close_connection(cnx):
+    cnx.close()
