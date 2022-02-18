5f93f043315eb4590d74474194cc2a790316fe07
Justin Liang <justinliang1020@gmail.com>
2022-02-18 14:39:42 -0500

    module based sql

--- sql_connection.py
+++ sql_connection.py
@@ -5,86 +5,68 @@ from mysql.connector import errorcode
 # 1. run CS3311.sql in mysql workbench
 #
 # 2. create a custom user with the credentials:
-#   user: 'cs3311_admin'
-#   password: 'password'
-#   administrative roles: DBA
-#   schema privileges: add entry -> schema 'CS 3311', Select "ALL" privileges
+#    user: 'cs3311_admin'
+#    password: 'password'
+#    administrative roles: DBA
+#    schema privileges: add entry -> schema 'CS 3311', Select "ALL" privileges
 #
-# 3. import and run sql_connection in another file. example code at the bottom
+# 3. import and run sql_connection in another file.
+#    example code found at sql_test.py
 
-
-
-
-class sql_connection:
-    def __init__(self, config):
-        self.config = config
-        self.cnx = None
-        self.cursor = None
-        self.connect()
-
-    def connect(self):
-        try:
-            self.cnx = mysql.connector.connect(**self.config)
-        except mysql.connector.Error as err:
-            if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
-                print("Something is wrong with your user name or password")
-            elif err.errno == errorcode.ER_BAD_DB_ERROR:
-                print("Database does not exist")
-            else:
-                print(err)
-
-        print('Database connection:', self.cnx)
-        self.cursor = self.cnx.cursor()
-        
-
-    def insert(self, table, data):
-        if table == 'projects':
-            query = ("INSERT INTO projects "
-               "(ProjectID, Project, State, Budget_Period, Reporting_Period) "
-               "VALUES (%(ProjectID)s, %(Project)s, %(State)s, %(Budget Period)s, %(Reporting Period)s);")
-        elif table == 'activities':
-            pass
-        if query:
-            self.cursor.execute(query, data)
-            self.cnx.commit()
-            #print('Insert executed on table:', table, '\nData:', data)
-        else:
-            print('Invalid table selected for insertion')
-
-
-    def close_connection(self):
-        if self.cnx:
-            print('Closed connection:', self.cnx)
-            self.cnx.close()
-            self.cursor.close()
-        else:
-            print('No connection to close')
-
-# import sql_connection
-# example usage
-# database connection config info
 config = {
     'user': 'cs3311_admin',
     'password': 'password',
     'host': '127.0.0.1',
     'database': 'CS3311'
 }
+cnx = None
+cursor = None
 
-# create a sql_connection object with config, this connects to the db
-sql = sql_connection(config)
-
-# create data to insert into db
-table = 'projects'
-data = {
-    'ProjectID': 104,
-    'Project': 'project name',
-    'State': 'Georgia',
-    'Budget Period': 'budget period dates',
-    'Reporting Period': 'reporting period dates'
+insert_query = {
+    'projects': ("INSERT INTO projects "
+            "(ProjectID, Project, State, Budget_Period, Reporting_Period) "
+            "VALUES (%(ProjectID)s, %(Project)s, %(State)s, %(Budget Period)s, %(Reporting Period)s);"),
+    'activities': ("INSERT INTO projects "
+            "(ID, Activity, Description, Outcome, Output, Timeline, Statistics, Status, Successes, Challenges, CDC_Support_Needed) "
+            "VALUES (%(ID)s, %(Activity)s, %(Description)s, %(Outcome)s, %(Output)s, %(Timeline)s, %(Statistics)s, %(Status)s, %(Successes)s, %(Challenges)s, %(CDC_Support_Needed)s);")
 }
 
-# insert data into db with given table and data
-sql.insert(table, data)
+def connect():
+    global cnx
+    global cursor
+    try:
+        cnx = mysql.connector.connect(**config)
+    except mysql.connector.Error as err:
+        if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+            print("Something is wrong with your user name or password")
+        elif err.errno == errorcode.ER_BAD_DB_ERROR:
+            print("Database does not exist")
+        else:
+            print(err)
 
-# close the connection
-sql.close_connection()
+    print('Database connection:', cnx)
+    cursor = cnx.cursor()
+    
+
+def insert(table, data):
+    if table in insert_query:
+        try:
+            cursor.execute(insert_query[table], data)
+            cnx.commit()
+        except mysql.connector.Error as err:
+            print("Insert failed: {}".format(err))
+    else:
+        print('Invalid table selected for insertion')
+
+def remove(table, data):
+    pass
+
+def close_connection():
+    if cnx:
+        print('Closed connection:', cnx)
+        cnx.close()
+        cursor.close()
+    else:
+        print('No connection to close')
+
+connect()
---
+++ sql_test.py
@@ -0,0 +1,19 @@
+import sql_connection
+
+# specify table to query
+table = 'projects'
+
+# specify data you want to query with
+data = {
+    'ProjectID': 112, # make sure primary key isn't duplicated!!
+    'Project': 'project name',
+    'State': 'Georgia',
+    'Budget Period': 'budget period dates',
+    'Reporting Period': 'reporting period dates'
+}
+
+# insert data into db with given table and data
+sql_connection.insert(table, data)
+
+# close the connection
+sql_connection.close_connection()
